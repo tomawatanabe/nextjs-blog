d30786d371976cd192dcfd8d10cf17bc54da1095
toma <tomawatanabe1997@gmail.com>
2022-11-08 12:23:48 +0900

    add stlyle

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,10 +1,17 @@
 import Link from "next/link";
+import Head from "next/head";
+import Layout from "../../components/layout";
 
 export default function FirstPost() {
   return (
     <>
-      <h1>First Post</h1>
-      <h2><Link legacyBehavior href="/"><a>Back to home</a></Link></h2>
+      <Layout>
+        <Head>
+          <title>First post</title>
+        </Head>
+        <h1>First Post</h1>
+        <h2><Link legacyBehavior href="/"><a>Back to home</a></Link></h2>
+      </Layout>
     </>
   )
 }
