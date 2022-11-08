56fd29fe7212bb6b9f1dfcbff825181c55bb84fa
toma <tomawatanabe1997@gmail.com>
2022-11-08 10:40:49 +0900

    setting Link component

---
+++ pages/posts/first-post.js
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function FirstPost() {
+  return (
+    <>
+      <h1>First Post</h1>
+      <h2><Link legacyBehavior href="/"><a>Back to home</a></Link></h2>
+    </>
+  )
+}
