81dbfc2942d9c4638860bb7bcf8ba657e16c9257
toma <tomawatanabe1997@gmail.com>
2022-11-10 17:54:24 +0900

    Polishing the Index Page

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,7 +3,8 @@ import Head from 'next/head';
 import Layout, { siteTitle } from '../components/layout';
 import utilStyles from '../styles/utils.module.css';
 import { getSortedPostsData } from '../lib/posts';
-
+import Link from 'next/link';
+import Date from '../components/date'
 
 export async function getStaticProps() {
   const allPostsData = getSortedPostsData();
@@ -42,11 +43,11 @@ const Home: FC<{
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }: any) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>{title}</Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
